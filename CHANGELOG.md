559231ff0b50cf2acfe733a96971af040ef1bd45
Chase Peterson <belax_8@hotmail.com>
2018-03-19 20:05:35 -0600

    Updated numberWithCommas to handle some edge cases.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 # CAP JS Helpers Change Log
 
 
+## v1.0.2
+### Fixes
+ * Fixed the numberWithCommas function to handle null values and values smaller than a thousandths
+
+
 ## v1.0.1
 ### Fixes
  * Updated deployed package to not include the circleci config
--- tests/number-formatter.test.js
+++ tests/number-formatter.test.js
@@ -14,6 +14,42 @@ describe('number-formatter', function () {
       expect(result).to.equal('1,234,567.89');
     });
 
+    it('should format number to string with commas without decimals', function () {
+      // Arrange
+      var num = 1234567890;
+      // Act
+      var result = numberFormatter.numberWithCommas(num);
+      // Assert
+      expect(result).to.equal('1,234,567,890');
+    });
+
+    it('should format number to string with commas when lots of decimal', function () {
+      // Arrange
+      var num = 1234.56789;
+      // Act
+      var result = numberFormatter.numberWithCommas(num);
+      // Assert
+      expect(result).to.equal('1,234.56789');
+    });
+
+    it('should format a zero correctly', function () {
+      // Arrange
+      var num = 0;
+      // Act
+      var result = numberFormatter.numberWithCommas(num);
+      // Assert
+      expect(result).to.equal('0');
+    });
+
+    it('should handle a null value', function () {
+      // Arrange
+      var num = null;
+      // Act
+      var result = numberFormatter.numberWithCommas(num);
+      // Assert
+      expect(result).to.equal('');
+    });
+
   });
   
 });
